ebcbe05b2ad4d4b0302c8077fa5c82727e537588
Le Duc <ductit1998@gmail.com>
2021-04-20 10:39:45 +0700

    Add MyDate File

---
+++ Date_Class/js/MyDate.js
@@ -0,0 +1,41 @@
+class MyDate {
+    constructor(day, month, year) {
+        this.day = day
+        this.month = month
+        this.year = year
+    }
+
+    getDay() {
+        return this.day
+    }
+
+    getMonth() {
+        return this.month
+    }
+
+    getYear() {
+        return this.year
+    }
+
+    setDay(day) {
+        this.day = day
+    }
+
+    setMonth(month) {
+        this.month = month
+    }
+
+    setYear(year) {
+        this.year = year
+    }
+
+    setDate(day, month, year) {
+        this.day = day
+        this.month = month
+        this.year = year
+    }
+    displayDate(){
+        alert(`${this.day}/${this.month}/${this.year}`)
+    }
+
+}
